72d3f7bfcb7a021dec16437d16aa179c56c01138
radmila01 <revarad95@gmail.com>
2023-04-26 23:31:55 +0600

    project create

--- server/index.js
+++ server/index.js
@@ -48,7 +48,7 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
 const start = async () => {
     try {
         await sequelize.authenticate()
-        await sequelize.sync({force: true})
+        await sequelize.sync({})
         app.listen(PORT, () => console.log('Server started'))
     } catch (e) {
         console.log(e)
